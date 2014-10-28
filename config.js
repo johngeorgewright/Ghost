847eef085976d9634725adc67b0d7cca353e07da
John Wright <johngeorge.wright@gmail.com>
2014-10-28 15:31:46 +0000

    Fixed the production URL.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog-j-g-w.info',
+        url: 'http://blog.j-g-w.info',
         mail: {
           transport: 'SMTP',
           host: 'smtp.mandrillapp.com',
